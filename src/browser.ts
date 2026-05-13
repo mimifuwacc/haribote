40b2f4140963f712e0f59616b40c6b6041863c51
mimifuwacc <mail@mimifuwa.cc>
2026-05-13 21:02:39 +0900

    chore: 不要なexportを削除

--- src/browser.ts
+++ src/browser.ts
@@ -1,5 +1,4 @@
 export { route, defineRoutes, defineConfig } from "./types";
 export { matchMeta, setMeta, fetchMeta } from "./client";
-export { resolveAndInject, resolveMeta } from "./resolver";
 export { createHandler } from "./handler";
 export type { MetaData, MetaSSROptions, RouteConfig, RouteContext, MetaResolver } from "./types";
